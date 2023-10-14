20f027bc2aff85bb3a4f389602dc042c2b02bb8f
Mason-Snyder <mgs22h@acu.edu>
2023-10-14 01:30:18 -0500

    completely fleshed abort and overide mechanics, line of sight is flawless, works very natrual. An acceptable completed state

--- Scurri/Assets/scripts/AIpatrol.cs
+++ Scurri/Assets/scripts/AIpatrol.cs
@@ -17,31 +17,56 @@ public class AIpatrol : MonoBehaviour
         homeDegree1 = 1.25f,
         homeDegree;
     private enum actions { search, go, track, seekSearch, none }
-    [SerializeField] LayerMask groundLayer, playerLayer; // for raycasting, confirming on navmesh and line of sight respectively
-    [SerializeField] private actions action;
+    [SerializeField] private LayerMask groundLayer, playerLayer; // for raycasting, confirming on navmesh and line of sight respectively
+    [SerializeField] private actions action, distAct, lastAction;
+    [SerializeField] private bool sightLine;
     private float targetDist;
-    private bool corou = true;
+    private bool corou = true; 
+    private RaycastHit2D hit;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player =  GameObject.Find("scurri'd");
+        playerLayer = LayerMask.GetMask("geometry", "player");
+        groundLayer = LayerMask.GetMask("navigation");
 
         agent.updateRotation = false;
         agent.updateUpAxis = false;
 
+        targetDist = Mathf.Infinity;
+
         StartCoroutine(getDist());
     }
+    private void FixedUpdate() // check line of sight to target
+    {
+        hit = Physics2D.Raycast(this.transform.position, (player.transform.position - this.transform.position).normalized, trackRadius, playerLayer);
+        if (LayerMask.LayerToName(hit.transform.gameObject.layer) == "player")
+            sightLine = true;
+        else
+            sightLine= false;
+    }
 
+    private void Update()
+    {
+        targetDist = Vector2.Distance(this.transform.position, player.transform.position);
+
+        if (targetDist < trackRadius && sightLine)
+            distAct = actions.track;
+        else if (targetDist < seekRadius1)
+            distAct = actions.seekSearch;
+        else
+            distAct = actions.search;
+        if (distAct != lastAction)
+            action = actions.none;
+    }
     IEnumerator getDist() // queue the action to be performed based on targetDist, and adjust behaviour config accordingly
     {
         while (corou)
         {
             if (action != actions.go)
             {
-                targetDist = Vector2.Distance(this.transform.position, player.transform.position);
-
-                if (Physics2D.Raycast(this.transform.position, (player.transform.position - this.transform.position).normalized, trackRadius, playerLayer)) // must stay as first if, checks line of sight
+                if (sightLine) // must stay as first if, checks line of sight
                     action = actions.track;
                 else if (targetDist < seekRadius1) // expandable via seekRadiusN/homeDegreeN
                 {
@@ -52,7 +77,7 @@ public class AIpatrol : MonoBehaviour
                     action = actions.search; // random from full range if player is not nearby
                 Patrol(); // generates new destination 
             }
-            else if (Vector3.Distance(transform.position, destPoint) < 1) // allows smooth motion and avoiding getting stuck, queues picking a new dest
+            else if (Vector3.Distance(transform.position, destPoint) < 2) // allows smooth motion and avoiding getting stuck, queues picking a new dest
                 action = actions.none;
 
             yield return new WaitForSeconds(.05f);
@@ -76,6 +101,7 @@ public class AIpatrol : MonoBehaviour
             case actions.track:
                 agent.SetDestination(player.transform.position); // very important that destPoint is not used here
                 action = actions.none;
+                lastAction = actions.track;
                 break;
             default:// should never trigger
                 action = actions.none;
@@ -92,6 +118,7 @@ public class AIpatrol : MonoBehaviour
                 action = actions.go;
         }
         while (action != actions.go);
+        lastAction = actions.search;
         Patrol(); // implement the new destPoint, important that is goes back to the switch and is not set here
     }
 
@@ -104,6 +131,7 @@ public class AIpatrol : MonoBehaviour
                 action = actions.go;
         }
         while (action != actions.go);
+        lastAction = actions.seekSearch;
         Patrol(); // same as above
     }
 }
