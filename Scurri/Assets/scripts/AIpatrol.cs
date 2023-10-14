eb6088350d5553746cdfbb1e54317282e5cbba42
Mason-Snyder <mgs22h@acu.edu>
2023-10-13 20:30:25 -0500

    Beautified code for easy expansion, added line-of-sight, and cleaned all numbers to public config variables

--- Scurri/Assets/scripts/AIpatrol.cs
+++ Scurri/Assets/scripts/AIpatrol.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,14 +9,18 @@ public class AIpatrol : MonoBehaviour
     GameObject player;
     NavMeshAgent agent;
 
-    private enum action {search, go, track}
-
-    [SerializeField] LayerMask groundLayer, playerLayer;
-
-    // patrol stuff
-    public Vector3 destPoint;
-    private bool walkpointSet;
-    [SerializeField] private float range = 20, targetDist;
+    public Vector2 destPoint;
+    public float // behaviour congif, can add numbered vars for additional radii levels without extra functions
+        range = 20,
+        trackRadius = 5,
+        seekRadius1 = 10,
+        homeDegree1 = 1.25f,
+        homeDegree;
+    private enum actions { search, go, track, seekSearch, none }
+    [SerializeField] LayerMask groundLayer, playerLayer; // for raycasting, confirming on navmesh and line of sight respectively
+    [SerializeField] private actions action;
+    private float targetDist;
+    private bool corou = true;
 
     void Start()
     {
@@ -28,36 +33,77 @@ public class AIpatrol : MonoBehaviour
         StartCoroutine(getDist());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator getDist() // queue the action to be performed based on targetDist, and adjust behaviour config accordingly
     {
-        Patrol();
+        while (corou)
+        {
+            if (action != actions.go)
+            {
+                targetDist = Vector2.Distance(this.transform.position, player.transform.position);
+
+                if (Physics2D.Raycast(this.transform.position, (player.transform.position - this.transform.position).normalized, trackRadius, playerLayer)) // must stay as first if, checks line of sight
+                    action = actions.track;
+                else if (targetDist < seekRadius1) // expandable via seekRadiusN/homeDegreeN
+                {
+                    action = actions.seekSearch;
+                    homeDegree = homeDegree1;
+                }
+                else
+                    action = actions.search; // random from full range if player is not nearby
+                Patrol(); // generates new destination 
+            }
+            else if (Vector3.Distance(transform.position, destPoint) < 1) // allows smooth motion and avoiding getting stuck, queues picking a new dest
+                action = actions.none;
+
+            yield return new WaitForSeconds(.05f);
+        }
+
     }
 
-    IEnumerator getDist()
+    void Patrol() // calls applicable destination-generating function
     {
-        targetDist = Vector3.Distance(this.transform.position, player.transform.position);
-        yield return new WaitForSeconds(0.5f);
+        switch (action)
+        {
+            case actions.search:
+                searchNorm();
+                break;
+            case actions.go:
+                agent.SetDestination(destPoint);
+                break;
+            case actions.seekSearch:
+                seekSearch();
+                break;
+            case actions.track:
+                agent.SetDestination(player.transform.position); // very important that destPoint is not used here
+                action = actions.none;
+                break;
+            default:// should never trigger
+                action = actions.none;
+                break;
+        }
     }
 
-    void Patrol()
+    void searchNorm() // full range allowed
     {
-        if (!walkpointSet) 
-            SearchForDest();
-        else if(walkpointSet)
-            agent.SetDestination(destPoint);
-        if(Vector3.Distance(transform.position, destPoint) < 1)
-            walkpointSet = false;
+        do
+        {
+            destPoint = new Vector2(transform.position.x + Random.Range(-range, range), transform.position.y + Random.Range(-range, range));
+            if (Physics2D.Raycast(destPoint, Vector2.zero, 1, groundLayer)) // check for being on navmesh
+                action = actions.go;
+        }
+        while (action != actions.go);
+        Patrol(); // implement the new destPoint, important that is goes back to the switch and is not set here
     }
 
-    void SearchForDest()
+    void seekSearch() // destPoint will be within a radius around the player that is itself a percentage of the distance between THIS and player (homeDegree)
     {
-        float y = Random.Range(-range, range);
-        float x = Random.Range(-range, range);
-
-        destPoint = new Vector3(transform.position.x + x, transform.position.y+y, transform.position.z);
-
-        if(Physics2D.Raycast(destPoint, Vector2.zero, 1, groundLayer) && (Vector3.Distance(destPoint, player.transform.position) <=  targetDist * 1.75))
-            walkpointSet = true;
+        do
+        {
+            destPoint = new Vector2(transform.position.x + Random.Range(-range, range), transform.position.y + Random.Range(-range, range));
+            if ((Vector2.Distance(destPoint, player.transform.position) <= targetDist * homeDegree) && Physics2D.Raycast(destPoint, Vector2.zero, 1, groundLayer))
+                action = actions.go;
+        }
+        while (action != actions.go);
+        Patrol(); // same as above
     }
 }
